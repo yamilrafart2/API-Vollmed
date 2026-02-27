f1f83c665e48b4c1e96b4e8d93b659a83bce9242
yamilrafart2 <yamilrafart2>
2026-02-27 20:08:33 -0300

    fix: asegura la presencia del ID en la actualización de médicos
    - Se agrega la anotación @NotNull al campo id en DatosActualizacionMedico.
    - Esta validación previene errores al intentar realizar actualizaciones parciales sin un identificador de recurso.
     Cambios a ser confirmados:
    	modificados:     src/main/java/med/voll/api/medico/DatosActualizacionMedico.java

--- src/main/java/med/voll/api/medico/DatosActualizacionMedico.java
+++ src/main/java/med/voll/api/medico/DatosActualizacionMedico.java
@@ -1,9 +1,10 @@
 package med.voll.api.medico;
 
+import jakarta.validation.constraints.NotNull;
 import med.voll.api.direccion.DatosDireccion;
 
 public record DatosActualizacionMedico(
-        Long id,
+        @NotNull Long id,
         String nombre,
         String telefono,
         DatosDireccion direccion
