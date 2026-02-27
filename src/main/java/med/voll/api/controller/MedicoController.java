f47f7d9bec75fbb7e72109358c14baa95169b62f
yamilrafart2 <yamilrafart2>
2026-02-27 19:09:02 -0300

    chore: configura logs de SQL y valores de paginación por defecto
    - Se habilitan las propiedades spring.jpa.show-sql y format_sql para visualizar las consultas en la consola.
    - Se implementa @PageableDefault en el listado de médicos para establecer un tamaño de página de 10 y ordenación por nombre de forma predeterminada.
     Cambios a ser confirmados:
    	modificados:     src/main/java/med/voll/api/controller/MedicoController.java
    	modificados:     src/main/resources/application.properties

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -8,6 +8,7 @@ import med.voll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -34,7 +35,7 @@ public class MedicoController {
     }
 
     @GetMapping
-    public Page<DatosListaMedico> listar(Pageable paginacion) {
+    public Page<DatosListaMedico> listar(@PageableDefault(size=10, sort={"nombre"}) Pageable paginacion) {
 
         return medicoRepository.findAll(paginacion).map(DatosListaMedico::new);
 
