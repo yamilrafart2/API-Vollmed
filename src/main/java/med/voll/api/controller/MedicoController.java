60e811ceda5e4c5a12c22b49db80989918075692
yamilrafart2 <yamilrafart2>
2026-02-27 18:54:07 -0300

    feat: implementa paginación y ordenamiento en el listado de médicos
    - Se reemplaza el retorno de List por Page en el método 'listar' del controlador.
    - Se integra el parámetro Pageable para permitir el control de registros desde la URL.
    - Se optimiza el mapeo a DTO aprovechando el método map nativo de la interfaz Page.
     Cambios a ser confirmados:
    	modificados:     src/main/java/med/voll/api/controller/MedicoController.java

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -6,6 +6,8 @@ import med.voll.api.medico.DatosRegistroMedico;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -32,11 +34,9 @@ public class MedicoController {
     }
 
     @GetMapping
-    public List<DatosListaMedico> listar() {
+    public Page<DatosListaMedico> listar(Pageable paginacion) {
 
-        return medicoRepository.findAll().stream()
-                .map(DatosListaMedico::new)
-                .toList();
+        return medicoRepository.findAll(paginacion).map(DatosListaMedico::new);
 
     }
 
