db3b9e70d535ae86e94946c5be8fb8c02ad12a98
yamilrafart2 <yamilrafart2>
2026-03-02 17:05:18 -0300

    feat: implementa GestorDeErrores global para manejo de 404
    - Se crea el RestControllerAdvice para capturar EntityNotFoundException.
    - Se centraliza el manejo de excepciones en el paquete infra.
     Cambios a ser confirmados:
    	nuevos archivos: src/main/java/med/voll/api/infra/GestorDeErrores.java

---
+++ src/main/java/med/voll/api/infra/GestorDeErrores.java
@@ -0,0 +1,18 @@
+package med.voll.api.infra;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GestorDeErrores {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity gestionarError404() {
+
+        return ResponseEntity.notFound().build();
+
+    }
+
+}
