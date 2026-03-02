e82fcff1bb854de0e1e3adca307db1c2a7619c4e
yamilrafart2 <yamilrafart2>
2026-03-02 17:27:01 -0300

    feat: personaliza manejo de errores de validación 400
    - Se añade manejador para MethodArgumentNotValidException en GestorDeErrores.
    - Se crea el record interno DatosErrorValidacion para simplificar la respuesta.
    - Se utiliza la API de Streams para mapear FieldErrors a un formato de respuesta limpio (campo y mensaje).
     Cambios a ser confirmados:
    	modificados:     src/main/java/med/voll/api/infra/GestorDeErrores.java

--- src/main/java/med/voll/api/infra/GestorDeErrores.java
+++ src/main/java/med/voll/api/infra/GestorDeErrores.java
@@ -2,6 +2,8 @@ package med.voll.api.infra;
 
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -12,6 +14,27 @@ public class GestorDeErrores {
     public ResponseEntity gestionarError404() {
 
         return ResponseEntity.notFound().build();
+
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity gestionarError400(MethodArgumentNotValidException ex) {
+
+        var errores = ex.getFieldErrors();
+        return ResponseEntity.badRequest().body(errores.stream()
+                .map(DatosErrorValidacion::new)
+                .toList()
+        );
+
+    }
+
+    public record DatosErrorValidacion(String campo, String mensaje) {
+
+        public DatosErrorValidacion(FieldError error) {
+
+            this(error.getField(), error.getDefaultMessage());
+
+        }
 
     }
 
