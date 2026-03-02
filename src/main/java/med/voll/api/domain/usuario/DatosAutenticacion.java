559efaffbd25502607cda8549ba47184d28f1385
yamilrafart2 <yamilrafart2>
2026-03-02 19:24:18 -0300

    feat: implementa endpoint de login y configuración de AuthenticationManager
    - Se expone el bean AuthenticationManager en la configuración de seguridad.
    - Se crea AutenticacionController con el endpoint /login para procesar credenciales.
    - Se implementa el record DatosAutenticacion para el manejo de datos de entrada.
     Cambios a ser confirmados:
    	nuevos archivos: src/main/java/med/voll/api/controller/AutenticacionController.java
    	nuevos archivos: src/main/java/med/voll/api/domain/usuario/DatosAutenticacion.java
    	modificados:     src/main/java/med/voll/api/infra/security/SecurityConfigurations.java

---
+++ src/main/java/med/voll/api/domain/usuario/DatosAutenticacion.java
@@ -0,0 +1,7 @@
+package med.voll.api.domain.usuario;
+
+public record DatosAutenticacion(
+        String login,
+        String contrasena
+) {
+}
--- src/main/java/med/voll/api/infra/security/SecurityConfigurations.java
+++ src/main/java/med/voll/api/infra/security/SecurityConfigurations.java
@@ -2,6 +2,8 @@ package med.voll.api.infra.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -17,6 +19,13 @@ public class SecurityConfigurations {
         return http.csrf(csrf -> csrf.disable())
                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .build();
+
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+
+        return configuration.getAuthenticationManager();
 
     }
 
