abf0f024342af67cf9cd4932b097de9dd57f9bff
yamilrafart2 <yamilrafart2>
2026-03-02 21:20:19 -0300

    feat: implementa validación de tokens JWT en el filtro de seguridad
    - Se agrega el método getSubject en TokenService para verificar la firma, el issuer y extraer el subject del token.
    - Se integra la validación en SecurityFilter llamando a tokenService.getSubject.
    - Se añade manejo de JWTVerificationException para capturar tokens inválidos o expirados.
     Cambios a ser confirmados:
    	modificados:     src/main/java/med/voll/api/infra/security/SecurityFilter.java
    	modificados:     src/main/java/med/voll/api/infra/security/TokenService.java

--- src/main/java/med/voll/api/infra/security/TokenService.java
+++ src/main/java/med/voll/api/infra/security/TokenService.java
@@ -3,6 +3,7 @@ package med.voll.api.infra.security;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import med.voll.api.domain.usuario.Usuario;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -35,6 +36,21 @@ public class TokenService {
     private Instant fechaExpiracion() {
 
         return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+
+    }
+
+    public String getSubject(String tokenJWT) {
+
+        try {
+            var algoritmo = Algorithm.HMAC256(secret);
+            return JWT.require(algoritmo)
+                    .withIssuer("API voll.med")
+                    .build()
+                    .verify(tokenJWT)
+                    .getSubject();
+        } catch (JWTVerificationException exception){
+            throw new RuntimeException("TOKEN JWT INVÁLIDO O EXPIRADO", exception);
+        }
 
     }
 
