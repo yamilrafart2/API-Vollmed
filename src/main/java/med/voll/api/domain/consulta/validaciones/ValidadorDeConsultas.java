e0890bcb63b1c0452029927cec74567efacc46a0
yamilrafart2 <yamilrafart2>
2026-03-04 20:33:29 -0300

    refactor: implementa sistema de validaciones usando el patrón Strategy
    - Se crea la interfaz ValidadorDeConsultas para estandarizar las reglas de negocio.
    - Se refactoriza ReservaDeConsultas para inyectar una lista de validadores y ejecutarlos dinámicamente.
    - Se marcan todos los validadores como @Component y se añade soporte para inyección de dependencias.
    - Este diseño permite la extensibilidad del sistema de reservas sin modificar el core del servicio.
     Cambios a ser confirmados:
    	modificados:     src/main/java/med/voll/api/domain/consulta/ReservaDeConsultas.java
    	modificados:     src/main/java/med/voll/api/domain/consulta/validaciones/ValidadorConsultaConAnticipacion.java
    	nuevos archivos: src/main/java/med/voll/api/domain/consulta/validaciones/ValidadorDeConsultas.java
    	modificados:     src/main/java/med/voll/api/domain/consulta/validaciones/ValidadorFueraHorarioConsultas.java
    	modificados:     src/main/java/med/voll/api/domain/consulta/validaciones/ValidadorMedicoActivo.java
    	modificados:     src/main/java/med/voll/api/domain/consulta/validaciones/ValidadorMedicoConOtraConsultaEnElMismoHorario.java
    	modificados:     src/main/java/med/voll/api/domain/consulta/validaciones/ValidadorPacienteActivo.java
    	modificados:     src/main/java/med/voll/api/domain/consulta/validaciones/ValidadorPacienteSinOtraConsultaElMismoDia.java

---
+++ src/main/java/med/voll/api/domain/consulta/validaciones/ValidadorDeConsultas.java
@@ -0,0 +1,9 @@
+package med.voll.api.domain.consulta.validaciones;
+
+import med.voll.api.domain.consulta.DatosReservaConsulta;
+
+public interface ValidadorDeConsultas {
+
+    void validar(DatosReservaConsulta datos);
+
+}
--- src/main/java/med/voll/api/domain/consulta/validaciones/ValidadorPacienteSinOtraConsultaElMismoDia.java
+++ src/main/java/med/voll/api/domain/consulta/validaciones/ValidadorPacienteSinOtraConsultaElMismoDia.java
@@ -2,13 +2,17 @@ package med.voll.api.domain.consulta.validaciones;
 
 import med.voll.api.domain.ValidacionException;
 import med.voll.api.domain.consulta.ConsultaRepository;
-import med.voll.api.domain.consulta.DatosDetalleConsulta;
+import med.voll.api.domain.consulta.DatosReservaConsulta;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
-public class ValidadorPacienteSinOtraConsultaElMismoDia {
+@Component
+public class ValidadorPacienteSinOtraConsultaElMismoDia implements  ValidadorDeConsultas {
 
+    @Autowired
     private ConsultaRepository consultaRepository;
 
-    public void validar(DatosDetalleConsulta datos) {
+    public void validar(DatosReservaConsulta datos) {
 
         var primerHorario = datos.fecha().withHour(7);
         var ultimoHorario = datos.fecha().withHour(18);
