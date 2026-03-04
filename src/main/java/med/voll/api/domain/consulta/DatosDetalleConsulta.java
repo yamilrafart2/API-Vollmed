141f566e7293f3d13b7b320f7bc5d1d5809e9ec8
yamilrafart2 <yamilrafart2>
2026-03-04 18:13:26 -0300

    feat: implementa estructura inicial para la gestión de consultas
    - Se crea el ConsultaController con el endpoint base para reservar citas.
    - Se implementa la entidad Consulta con relaciones ManyToOne hacia Medico y Paciente (Lazy Fetch).
    - Se crean los DTOs DatosReservaConsulta (con validación @Future) y DatosDetalleConsulta.
    - Se añade la migración V8 para la creación de la tabla 'consultas' con sus llaves foráneas.
     Cambios a ser confirmados:
    	nuevos archivos: src/main/java/med/voll/api/controller/ConsultaController.java
    	nuevos archivos: src/main/java/med/voll/api/domain/consulta/Consulta.java
    	nuevos archivos: src/main/java/med/voll/api/domain/consulta/ConsultaRepository.java
    	nuevos archivos: src/main/java/med/voll/api/domain/consulta/DatosDetalleConsulta.java
    	nuevos archivos: src/main/java/med/voll/api/domain/consulta/DatosReservaConsulta.java
    	nuevos archivos: src/main/resources/db/migration/V8__create-table-consultas.sql

---
+++ src/main/java/med/voll/api/domain/consulta/DatosDetalleConsulta.java
@@ -0,0 +1,11 @@
+package med.voll.api.domain.consulta;
+
+import java.time.LocalDateTime;
+
+public record DatosDetalleConsulta(
+        Long id,
+        Long idMedico,
+        Long idPaciente,
+        LocalDateTime fecha
+) {
+}
---
+++ src/main/resources/db/migration/V8__create-table-consultas.sql
@@ -0,0 +1,10 @@
+CREATE TABLE consultas(
+    id BIGINT NOT NULL auto_increment,
+    medico_id BIGINT NOT NULL,
+    paciente_id BIGINT NOT NULL,
+    fecha DATETIME NOT NULL,
+
+    PRIMARY KEY(id),
+    CONSTRAINT fk_consultas_medico_id FOREIGN KEY(medico_id) REFERENCES medicos(id),
+    CONSTRAINT fk_consultas_paciente_id FOREIGN KEY(paciente_id) REFERENCES pacientes(id)
+);
