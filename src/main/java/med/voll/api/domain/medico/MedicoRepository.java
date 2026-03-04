75666bad8ec3de8fd0b8c672655ac7eb7427236c
yamilrafart2 <yamilrafart2>
2026-03-04 20:21:04 -0300

    feat: añade consultas personalizadas a los repositorios de médicos y pacientes
    - Se implementa lógica para elegir médico aleatorio disponible en una fecha específica.
    - Se añaden métodos para verificar el estado activo de médicos y pacientes por ID.
     Cambios a ser confirmados:
    	modificados:     src/main/java/med/voll/api/domain/medico/MedicoRepository.java
    	modificados:     src/main/java/med/voll/api/domain/paciente/PacienteRepository.java

--- src/main/java/med/voll/api/domain/medico/MedicoRepository.java
+++ src/main/java/med/voll/api/domain/medico/MedicoRepository.java
@@ -1,11 +1,40 @@
 package med.voll.api.domain.medico;
 
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.time.LocalDateTime;
 
 public interface MedicoRepository extends JpaRepository<Medico, Long> {
 
     Page<Medico> findByActivoTrue(Pageable paginacion);
 
+    @Query("""
+            SELECT m FROM Medico m
+            WHERE
+            m.activo = 1
+            AND
+            m.especialidad = :especialidad
+            AND
+            m.id NOT IN(
+                SELECT c.medico.id FROM Consulta c
+                WHERE
+                c.fecha = :fecha
+            )
+            ORDER BY rand()
+            LIMIT 1 
+    """)
+    Medico elegirMedicoAleatorioDisponibleEnLaFecha(Especialidad especialidad, @NotNull @Future LocalDateTime fecha);
+
+    @Query("""
+            SELECT m.activo
+            FROM Medico m
+            WHERE
+            m.id = :idMedico
+            """)
+    boolean findActivoById(Long idMedico);
 }
--- src/main/java/med/voll/api/domain/paciente/PacienteRepository.java
+++ src/main/java/med/voll/api/domain/paciente/PacienteRepository.java
@@ -5,5 +5,9 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PacienteRepository extends JpaRepository<Paciente, Long> {
+
     Page<Paciente> findAllByActivoTrue(Pageable paginacion);
+
+    boolean findActivoById(Long idPaciente);
+
 }
