763a9704410bc08831898ed22b180ccc260ab2a9
yamilrafart2 <yamilrafart2>
2026-03-04 20:17:50 -0300

    feat: expone endpoints de reserva y cancelación en el controlador
    - Se añade el método cancelar en ConsultaController.
    - Se conecta el controlador con el servicio ReservaDeConsultas.
    - Se actualizan los DTOs para incluir especialidad y motivos de cancelación.
     Cambios a ser confirmados:
    	modificados:     src/main/java/med/voll/api/controller/ConsultaController.java
    	nuevos archivos: src/main/java/med/voll/api/domain/consulta/DatosCancelamientoConsulta.java
    	modificados:     src/main/java/med/voll/api/domain/consulta/DatosReservaConsulta.java

--- src/main/java/med/voll/api/controller/ConsultaController.java
+++ src/main/java/med/voll/api/controller/ConsultaController.java
@@ -1,26 +1,38 @@
 package med.voll.api.controller;
 
 import jakarta.validation.Valid;
+import med.voll.api.domain.consulta.DatosCancelamientoConsulta;
 import med.voll.api.domain.consulta.DatosDetalleConsulta;
 import med.voll.api.domain.consulta.DatosReservaConsulta;
+import med.voll.api.domain.consulta.ReservaDeConsultas;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("consultas")
 public class ConsultaController {
+
+    @Autowired
+    private ReservaDeConsultas reserva;
 
     @PostMapping
     @Transactional
     public ResponseEntity reservar(@RequestBody @Valid DatosReservaConsulta datos) {
 
         System.out.println("\ndatos\n");
+
+        reserva.reservar(datos);
         return ResponseEntity.ok(new DatosDetalleConsulta(null, null, null, null));
 
+    }
+
+    @DeleteMapping
+    @Transactional
+    public ResponseEntity cancelar(@RequestBody @Valid DatosCancelamientoConsulta datos) {
+        reserva.cancelar(datos);
+        return ResponseEntity.noContent().build();
     }
 
 }
