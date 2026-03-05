38f00ff93038c737426de9223f7d6221520dc3b3
yamilrafart2 <yamilrafart2>
2026-03-05 19:18:52 -0300

    docs: personaliza información general de la API en OpenAPI
    - Se añade el objeto Info al bean customOpenAPI.
    - Se define el título, descripción detallada y contacto del equipo backend.
    - Se incluye la licencia de uso Apache 2.0.
    - Estos cambios mejoran la presentación visual en el encabezado de Swagger UI.
     Cambios a ser confirmados:
    	modificados:     src/main/java/med/voll/api/infra/springdoc/SpringDocConfiguration.java

--- src/main/java/med/voll/api/infra/springdoc/SpringDocConfiguration.java
+++ src/main/java/med/voll/api/infra/springdoc/SpringDocConfiguration.java
@@ -2,6 +2,9 @@ package med.voll.api.infra.springdoc;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,8 +16,20 @@ public class SpringDocConfiguration {
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .components(new Components()
-                .addSecuritySchemes("bearer-key",
-                new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")));
+                        .addSecuritySchemes("bearer-key",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")))
+                .info(new Info()
+                        .title("Voll.med API")
+                        .description("API Rest de la aplicación Voll.med, que contiene las funcionalidades CRUD de médicos y de pacientes, además de reserva y cancelación de consultas")
+                        .contact(new Contact()
+                                .name("Equipo Backend")
+                                .email("backend@voll.med"))
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("http://voll.med/api/licencia")));
     }
 
 
