1ef10f14fcbe6e691221e3720dbbbec0742c56a2
yamilrafart2 <yamilrafart2>
2026-03-06 00:34:38 -0300

    test: implementa escenario de éxito HTTP 200 en ConsultaController
    - Se agrega el test reservar_escenario2 para simular una petición POST con un JSON válido.
    - Se incorpora @AutoConfigureJsonTesters y JacksonTester para la conversión automática de objetos a JSON.
    - Se utiliza @MockBean para simular (mockear) el servicio ReservaDeConsultas y aislar la prueba del acceso a la base de datos.
    - Se verifica que el controlador devuelva HTTP 200 (OK) y que el cuerpo de la respuesta coincida con el JSON esperado.
    - Se corrige error tipográfico en el nombre del método del escenario 1.
     Cambios a ser confirmados:
    	modificados:     src/test/java/med/voll/api/controller/ConsultaControllerTest.java

--- src/test/java/med/voll/api/controller/ConsultaControllerTest.java
+++ src/test/java/med/voll/api/controller/ConsultaControllerTest.java
@@ -1,32 +1,78 @@
 package med.voll.api.controller;
 
+import med.voll.api.domain.consulta.DatosDetalleConsulta;
+import med.voll.api.domain.consulta.DatosReservaConsulta;
+import med.voll.api.domain.consulta.ReservaDeConsultas;
+import med.voll.api.domain.medico.Especialidad;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.time.LocalDateTime;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@AutoConfigureJsonTesters
 class ConsultaControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    private JacksonTester<DatosReservaConsulta> datosReservaConsultaJson;
+
+    @Autowired
+    private JacksonTester<DatosDetalleConsulta> datosDetalleConsultaJson;
+
+    @MockBean
+    private ReservaDeConsultas reservaDeConsultas;
+
     @Test
     @DisplayName("DEBERÍA DEVOLVER HTTP 400 CUÁNDO LA REQUEST NO TENGA DATOS")
     @WithMockUser
-    void reservar_ecenario1() throws Exception {
+    void reservar_escenario1() throws Exception {
 
         var response = mockMvc.perform(post("/consultas"))
                 .andReturn().getResponse();
 
         assertThat(response.getStatus()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+    }
+
+    @Test
+    @DisplayName("DEBERÍA DEVOLVER HTTP 200 CUÁNDO LA REQUEST RECIBA UN JSON VÁLIDO")
+    @WithMockUser
+    void reservar_escenario2() throws Exception {
+
+        var fecha = LocalDateTime.now().plusHours(1);
+        var especialidad = Especialidad.CARDIOLOGIA;
+        var datosDetalle = new DatosDetalleConsulta(null, 2l, 5l, fecha);
+        when(reservaDeConsultas.reservar(any())).thenReturn(datosDetalle);
+
+        var response = mockMvc.perform(post("/consultas")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(datosReservaConsultaJson.write(
+                                new DatosReservaConsulta(2l, 5l, fecha, especialidad)
+                        ).getJson()))
+                .andReturn().getResponse();
+
+        var jsonEsperado = datosDetalleConsultaJson.write(
+                datosDetalle
+        ).getJson();
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
+        assertThat(response.getContentAsString()).isEqualTo(jsonEsperado);
     }
 }
